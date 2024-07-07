4279055dd72636f919ea36d7c3ce36ca49e24c76
seaung <seaung.c0m7de@protonmail.com>
2024-07-07 23:02:23 +0800

    :beers: 添加模型和依赖

--- NMAX/apps/users/models.py
+++ NMAX/apps/users/models.py
@@ -3,7 +3,27 @@ from django.utils.translation import gettext_lazy as _
 from django.utils.timezone import now
 from django.db import models
 
+from solo.models import SingletonModel
+
 # Create your models here.
+
+
+LOGIN_TIMEOUT = 3 * 60 * 60
+
+LOGIN_ATTEMPT_TIMEOUT = 3 * 60
+
+LOGIN_MAX_ATTEMPT_COUNT = 5
+
+LOGIN_LOCK_TIME = 5 * 60
+
+
+class LoginStrategy(SingletonModel):
+    login_timeout = models.IntegerField(default=LOGIN_TIMEOUT)
+    attempt_time_window = models.IntegerField(default=LOGIN_ATTEMPT_TIMEOUT)
+    max_attempt_count = models.IntegerField(default=LOGIN_MAX_ATTEMPT_COUNT)
+    lock_mode = models.SmallIntegerField()
+    lock_time = models.IntegerField(default=LOGIN_LOCK_TIME)
+    multi_client_forbidden = models.BooleanField(default=False)
 
 
 class Role(models.Model):
